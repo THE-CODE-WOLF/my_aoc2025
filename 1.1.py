f954681840c90c2f37beee40b71adaec002942af
THE-CODE-WOLF <woodsbraydenj@gmail.com>
2025-12-02 11:16:03 -0500

    Day 1 both puzzles

---
+++ 1.1.py
@@ -0,0 +1,38 @@
+import re
+
+fileName = "Puzzles/day1"
+
+with open(fileName, encoding="utf-8") as file:
+    lines = [line.rstrip() for line in file]
+
+test_lines = ["L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82", "L1000"]
+
+dial_num = 50
+count = 0
+
+for line in lines:
+
+    direction = re.findall("[LR]", str(line))
+    amount = re.findall("\d+", str(line))
+    amount = int(amount[0])
+
+    print(direction, amount)
+
+    if direction[0] == "L":
+        amount *= -1
+
+    dial_num += amount
+
+    dial_num %= 100
+
+    print(dial_num)
+
+    if dial_num == 0:
+        count += 1
+        print("+")
+
+print(count)
+
+
+
+
---
+++ 1.2.py
@@ -0,0 +1,42 @@
+import re
+import math
+
+fileName = "Puzzles/day1"
+
+with open(fileName, encoding="utf-8") as file:
+    lines = [line.rstrip() for line in file]
+
+dial_num = 50
+count = 0
+
+test_lines = ["L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82", "L1000"]
+
+for line in lines:
+
+    #print(line)
+
+    direction = re.findall("[LR]", line)[0]
+    amount = int(re.findall(r"\d+", line)[0])
+
+    if direction == "L":
+        amount *= -1
+
+    start = dial_num
+    end = dial_num + amount
+
+    #print(direction, amount)
+
+    if amount > 0:
+
+        loops = (end // 100) - (start // 100)
+    elif amount < 0:
+
+        loops = ((start - 1) // 100) - ((end - 1) // 100)
+    else:
+        loops = 0
+
+    count += loops
+
+    dial_num = end % 100
+
+print(count)
