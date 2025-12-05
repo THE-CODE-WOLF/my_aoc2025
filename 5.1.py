443554ee0eb0322484e50f315c0efe1e77566ab9
THE-CODE-WOLF <woodsbraydenj@gmail.com>
2025-12-05 18:30:33 -0500

    Started day 5

---
+++ 5.1.py
@@ -0,0 +1,8 @@
+fileName = "Puzzles/day5"
+
+with open(fileName, encoding="utf-8") as file:
+    lines = [line.rstrip() for line in file]
+
+lines = ["3-5", "10-14", "16-20", "12-18", "1", "5", "8", "11", "17", "32"]
+
+
